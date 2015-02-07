15bb4192994484d0f966dce1e413045324a2265e
Kristof Van Cauwenbergh <kristof@king-foo.be>
2015-02-07 13:31:12 +0100

    Append snippets for the first scenario in the project manager context

--- features/bootstrap/ProjectManagerContext.php
+++ features/bootstrap/ProjectManagerContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
@@ -19,5 +20,45 @@ class ProjectManagerContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given a sprint named :arg1 with a duration of :arg2 hours
+     */
+    public function aSprintNamedWithADurationOfHours($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given a task :arg1 with an estimate of :arg2 hours
+     */
+    public function aTaskWithAnEstimateOfHours($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I choose to assign the task :arg1 to the given sprint
+     */
+    public function iChooseToAssignTheTaskToTheGivenSprint($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the given sprint should be filled with :arg1 task
+     */
+    public function theGivenSprintShouldBeFilledWithTask($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the given sprint estimate should be :arg1 hours
+     */
+    public function theGivenSprintEstimateShouldBeHours($arg1)
+    {
+        throw new PendingException();
     }
 }
